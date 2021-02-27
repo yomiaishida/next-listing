fac0fc6919f6317f6530953356c80e4ced833c3a
Abayomi Aishida <yomicorperate@gmail.com>
2021-02-27 15:53:17 +0100

    Fetching data from jsonplaceholder

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,26 @@
 import React from "react";
+import styles from "../../styles/Ninjas.module.css";
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
